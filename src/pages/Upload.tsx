fbe16363197c9c939f86ce4b5d21a37aa3615624
kkyuheak <gyuheak@gmail.com>
2024-08-26 00:27:04 +0900

    upload page 디자인 완성

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -1,8 +1,84 @@
 import { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import styled from "styled-components";
+import Button from "../components/Button";
 
-const Wrapper = styled.form``;
+const Wrapper = styled.form`
+  max-width: 1200px;
+  margin: 10px auto;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 1px 0px 100px rgb(180, 180, 180);
+`;
+
+const Title = styled.h1`
+  font-size: 35px;
+  font-weight: 600;
+  font-family: "Noto Serif KR", serif;
+`;
+const Label = styled.label`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 35px;
+  color: #fff;
+  background-color: rgb(14, 127, 244);
+  border-radius: 10px;
+  margin: 10px 0;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgba(14, 127, 244, 0.8);
+  }
+`;
+
+const Input = styled.input`
+  display: none;
+`;
+
+const PrevImages = styled.div`
+  width: 100%;
+  height: 560px;
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  overflow-x: scroll;
+  border: 1px solid #b7b7b7;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const PrevImg = styled.div<{ $src: string }>`
+  background-image: url(${(props) => props.$src});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-color: #fff;
+  border-radius: 10px;
+  width: 400px;
+  height: 550px;
+  flex-shrink: 0;
+  box-shadow: 1px 0px 10px rgb(180, 180, 180);
+`;
+
+const Text = styled.textarea`
+  display: block;
+  margin-top: 20px;
+  border: 1px solid #cacaca;
+  border-radius: 5px;
+  font-size: 16px;
+  width: 100%;
+  height: 300px;
+  outline: none;
+  padding: 10px;
+  resize: none;
+  font-family: "Noto Serif KR", serif;
+`;
 
 interface IFormInput {
   images: FileList;
@@ -14,6 +90,7 @@ const Upload = () => {
 
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
     console.log(data);
+    console.log(imgPreviews);
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -26,38 +103,28 @@ const Upload = () => {
     }
   };
 
-  // useEffect(() => {
-  //   if(imagesFiles && imagesFiles.length > 0){
-  //     const previews: string[] = [];
-  //     for(let i = 0; i < imagesFiles.length; i++){
-  //       const reader = new FileReader();
-  //       reader.onloadend  = () => {
-  //         previews.push(reader.result + "");
-  //         if(previews.length === imgPreviews.length){
-  //           setImgPreviews(previews)
-  //         }
-  //       }
-  //       reader.readAsDataURL(imagesFiles[i])
-  //     }
-  //   }
-  // }, [imagesFiles])
-
   return (
     <Wrapper onSubmit={handleSubmit(onSubmit)}>
-      <h1>새 게시물 만들기</h1>
-      <input
+      <Title>새 게시물 만들기</Title>
+      <Label htmlFor="img">사진 추가</Label>
+      <Input
+        id="img"
         type="file"
         accept=".png, .jpeg, .jpg"
         {...register("images")}
         onChange={handleChange}
         multiple
       />
-      <div>
+      <PrevImages>
         {imgPreviews.map((preview, i) => {
-          return <img src={preview} key={i} />;
+          return <PrevImg $src={preview} key={i} />;
         })}
-      </div>
-      <button>등록하기</button>
+      </PrevImages>
+      <Text
+        placeholder="문구를 작성하거나 설명을 추가하세요 (최대 400자)"
+        maxLength={400}
+      ></Text>
+      <Button width="120px">등록하기</Button>
     </Wrapper>
   );
 };
