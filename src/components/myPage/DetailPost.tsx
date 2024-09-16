db98d697e85acbea58bacd9976f243c24a2fecc4
kkyuheak <gyuheak@gmail.com>
2024-09-16 18:37:56 +0900

    DetailPost 스크롤 금지 개발중, 닫기 버튼 구현완료

--- src/components/myPage/DetailPost.tsx
+++ src/components/myPage/DetailPost.tsx
@@ -8,12 +8,14 @@ import { Navigation, Pagination } from "swiper/modules";
 import { useRecoilValue } from "recoil";
 import { UserAtom } from "../../atom";
 import UserIcon from "../user/UserIcon";
+import { IoClose } from "react-icons/io5";
+import { useEffect } from "react";
 
 const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.7);
-  position: absolute;
+  position: fixed;
   inset: 0;
   display: flex;
   align-items: center;
@@ -23,7 +25,7 @@ const Wrapper = styled.div`
 const Post = styled.div`
   background-color: #fff;
   width: 70%;
-  height: 90%;
+  height: 95%;
   display: flex;
 `;
 
@@ -40,7 +42,8 @@ const Image = styled.div<{ $src: string }>`
   background-repeat: no-repeat;
   background-position: center;
   background-size: contain;
-  background-color: pink;
+  background-color: #fff;
+  border: 1px solid #cdcdcd;
 `;
 
 const SwiperStyle = styled(Swiper)`
@@ -87,7 +90,50 @@ const UserName = styled.p`
   cursor: pointer;
 `;
 
-const Mid = styled.div``;
+const Mid = styled.div`
+  height: 80%;
+`;
+
+const PostDescription = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+  gap: 10px;
+  height: 60px;
+`;
+
+const Desc = styled.p``;
+
+const Comments = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 0 15px;
+  background-color: #fff;
+  height: 55px;
+  margin-top: 10px;
+`;
+
+const Comment = styled.p`
+  font-size: 14px;
+`;
+
+const CloseBtn = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: #c4c4c4;
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  transition: all 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #fff;
+    transition: all 0.3s;
+  }
+`;
 
 interface IDetailPostProps {
   postInfo: IPostData;
@@ -102,6 +148,16 @@ const DetailPost = ({ postInfo, setDetailOpen }: IDetailPostProps) => {
   const handlePostClick = (e: React.MouseEvent) => {
     e.stopPropagation();
   };
+
+  useEffect(() => {
+    window.addEventListener("wheel", (e) => e.preventDefault(), {
+      passive: false,
+    });
+
+    return () => {
+      window.addEventListener("wheel", (e) => e.preventDefault());
+    };
+  }, []);
 
   return (
     <Wrapper onClick={setDetailOpen}>
@@ -122,15 +178,35 @@ const DetailPost = ({ postInfo, setDetailOpen }: IDetailPostProps) => {
             })}
           </SwiperStyle>
         </PostImages>
+
         {/* infoBox */}
         <Info>
+          {/* 게시글 유저 정보 */}
           <Top>
             <UserIcon userProfileSrc="/images/userIcon.png" />
             <UserName>{userinfo.nickname}</UserName>
           </Top>
-          <Mid></Mid>
+
+          <Mid>
+            {/* 게시글 설명 */}
+            <PostDescription>
+              <UserIcon userProfileSrc="/images/userIcon.png" />
+              <UserName>{userinfo.nickname}</UserName>
+              <Desc>{postInfo.description}</Desc>
+            </PostDescription>
+
+            {/* 임시 댓글 */}
+            <Comments>
+              <UserIcon userProfileSrc="/images/userIcon.png" />
+              <UserName>{userinfo.nickname}</UserName>
+              <Comment>댓글1</Comment>
+            </Comments>
+          </Mid>
         </Info>
       </Post>
+      <CloseBtn>
+        <IoClose size={40} />
+      </CloseBtn>
     </Wrapper>
   );
 };
